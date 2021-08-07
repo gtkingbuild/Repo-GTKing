556512886a6519d1f195e27ec389c6325efcb4da
gtkingbuild <gtkingbuild@gmail.com>
2021-08-08 01:06:40 +0200

    Update Repo

--- matrix/plugin.program.autowidget/resources/lib/refresh.py
+++ matrix/plugin.program.autowidget/resources/lib/refresh.py
@@ -1,12 +1,16 @@
 import xbmc
 import xbmcgui
 
+import os
 import random
 import time
 import threading
 
 from resources.lib import manage
+from resources.lib.common import settings
 from resources.lib.common import utils
+
+_addon_data = utils.translate_path(settings.get_addon_info("profile"))
 
 skin_string_pattern = "autowidget-{}-{}"
 _properties = ["context.autowidget"]
@@ -33,7 +37,7 @@ class RefreshService(xbmc.Monitor):
 
     def _update_properties(self):
         for property in _properties:
-            setting = utils.get_setting(property)
+            setting = settings.get_setting(property)
             utils.log("{}: {}".format(property, setting))
             if setting is not None:
                 utils.set_property(property, setting)
@@ -45,12 +49,12 @@ class RefreshService(xbmc.Monitor):
         self._reload_settings()
 
     def _reload_settings(self):
-        self.refresh_enabled = utils.get_setting_int("service.refresh_enabled")
-        self.refresh_duration = utils.get_setting_float("service.refresh_duration")
-        self.refresh_notification = utils.get_setting_int(
+        self.refresh_enabled = settings.get_setting_int("service.refresh_enabled")
+        self.refresh_duration = settings.get_setting_float("service.refresh_duration")
+        self.refresh_notification = settings.get_setting_int(
             "service.refresh_notification"
         )
-        self.refresh_sound = utils.get_setting_bool("service.refresh_sound")
+        self.refresh_sound = settings.get_setting_bool("service.refresh_sound")
 
         utils.update_container(True)
 
@@ -97,6 +101,11 @@ class RefreshService(xbmc.Monitor):
                     if not widget_def:
                         continue
                     _update_strings(widget_def)
+                if (
+                    os.path.exists(os.path.join(_addon_data, "refresh.time"))
+                    and utils.get_active_window() == "home"
+                ):
+                    utils.update_container(True)
 
             if self.abortRequested():
                 break
@@ -147,29 +156,23 @@ def update_path(widget_id, target, path=None):
 
     if target == "next" and path:
         utils.log("Next Page selected from {}".format(widget_id), "debug")
-        path_def = widget_def["path"]
+        path_def = manage.get_path_by_id(widget_def["path"], widget_def["group"])
         if isinstance(path_def, dict):
             widget_def["label"] = path_def["label"]
 
-        stack.append(widget_def["path"])
+        stack.append(path)
         widget_def["stack"] = stack
-        widget_def["path"] = path
     elif target == "back" and widget_def.get("stack"):
         utils.log("Previous Page selected from {}".format(widget_id), "debug")
-        widget_def["path"] = widget_def["stack"][-1]
         widget_def["stack"] = widget_def["stack"][:-1]
-
-        if len(widget_def["stack"]) == 0:
-            widget_def["label"] = ""
     elif target == "reset":
         if len(stack) > 0:
-            widget_def["path"] = widget_def["stack"][0]
-            widget_def["stack"] = []
-            widget_def["label"] = ""
+            # simple compatibility with pre-3.3.0 widgets
+            if isinstance(stack[0], dict):
+                widget_def["path"] = stack[0].get("id", "")
 
-    action = widget_def["path"] if widget_def["action"] != "merged" else "merged"
-    if isinstance(widget_def["path"], dict):
-        action = widget_def["path"]["file"]["file"]
+            widget_def["stack"] = []
+
     manage.save_path_details(widget_def)
     _update_strings(widget_def)
     utils.update_container(True)
@@ -194,34 +197,43 @@ def refresh(widget_id, widget_def=None, paths=None, force=False, single=False):
     current_time = time.time()
     updated_at = widget_def.get("updated", 0)
 
-    default_refresh = utils.get_setting_float("service.refresh_duration")
+    default_refresh = settings.get_setting_float("service.refresh_duration")
     refresh_duration = float(widget_def.get("refresh", default_refresh))
 
     if updated_at <= current_time - (3600 * refresh_duration) or force:
         group_id = widget_def["group"]
         action = widget_def.get("action")
-        current = int(widget_def.get("current", -1))
+        current = int(widget_def.get("current", 0))
         widget_def["stack"] = []
-        widget_def["label"] = ""
 
         if not paths:
-            paths = manage.find_defined_paths(group_id)
+            cycle_paths = widget_def.get("cycle_paths")
+            if cycle_paths is None:
+                cycle_paths = [p.get("id") for p in manage.find_defined_paths(group_id)]
+                widget_def["cycle_paths"] = cycle_paths
+
+            paths = [p for p in cycle_paths]
 
         if action:
             if len(paths) > 0:
                 next = 0
                 if action == "next":
                     next = (current + 1) % len(paths)
+
                 elif action == "random":
                     random.shuffle(paths)
                     next = random.randrange(len(paths))
 
                 widget_def["current"] = next
-                path_def = paths[next]
+                path_id = paths[next]
                 paths.remove(paths[next])
 
-                widget_def["path"] = path_def
+                widget_def["path"] = path_id
                 if widget_def["path"]:
+                    path_label = manage.get_path_by_id(path_id, group_id).get(
+                        "label", ""
+                    )
+                    widget_def["label"] = path_label
                     widget_def["updated"] = 0 if force else current_time
 
                     manage.save_path_details(widget_def)
@@ -239,8 +251,9 @@ def refresh_paths(notify=False, force=False):
         dialog.notification(
             "AutoWidget",
             utils.get_string(30020),
-            sound=utils.get_setting_bool("service.refresh_sound"),
+            sound=settings.get_setting_bool("service.refresh_sound"),
         )
+        del dialog
 
     for group_def in manage.find_defined_groups():
         paths = []
@@ -265,29 +278,52 @@ def get_files_list(path, widget_id=None):
         files, changed = utils.cache_files(path, widget_id)
 
     new_files = []
-    if "error" not in files:
-        files = files.get("result").get("files")
+    if "result" in files:
+        files = files.get("result", {}).get("files", [])
         if not files:
             utils.log("No items found for {}".format(path))
-            return
+            return [], hash
 
         for file in files:
-            new_file = {
-                k: v for k, v in file.items() if v not in [None, "", -1, [], {}]
-            }
+            new_file = {k: v for k, v in file.items() if v is not None}
+
             if "art" in new_file:
                 for art in new_file["art"]:
                     new_file["art"][art] = utils.clean_artwork_url(file["art"][art])
+            if "cast" in new_file:
+                for idx, cast in enumerate(new_file["cast"]):
+                    new_file["cast"][idx]["thumbnail"] = utils.clean_artwork_url(
+                        cast.get("thumbnail", "")
+                    )
             new_files.append(new_file)
 
-        return new_files
+        return new_files, hash
+    elif "error" in files:
+        os.remove(os.path.join(_addon_data, "{}.cache".format(hash)))
+        utils.log("Invalid cache file removed for {}".format(hash))
+        return None, hash
+    else:
+        utils.log("Error processing {}".format(hash), "error")
+        return None, hash
+
+
+def queue_widget_update(widget_id):
+    global _thread
+    new_thread = False
+    if _thread is None or not _thread.is_alive():
+        _thread = Worker()
+        _thread.daemon = True
+        new_thread = True
+    _thread.queue.put(widget_id)
+    if new_thread:
+        _thread.start()
 
 
 def is_duplicate(title, titles):
-    if not utils.get_setting_bool("widgets.hide_duplicates"):
+    if not settings.get_setting_bool("widgets.hide_duplicates"):
         return False
 
-    prefer_eps = utils.get_setting_bool("widgets.prefer_episodes")
+    prefer_eps = settings.get_setting_bool("widgets.prefer_episodes")
     if title["type"] == "movie":
         return (title["label"], title["imdbnumber"]) in [
             (t["label"], t["imdbnumber"]) for t in titles
