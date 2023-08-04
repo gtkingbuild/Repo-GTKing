c42633d1e39fddd9ab4bc8f39cf1b3252cb30055
gtkingbuild <gtkingbuild@gmail.com>
2023-08-04 16:04:56 +0200

    1

--- nexus/plugin.program.GTKing-Phoenix-Nexus/resources/libs/common/config.py
+++ nexus/plugin.program.GTKing-Phoenix-Nexus/resources/libs/common/config.py
@@ -315,10 +315,10 @@ class Config:
             use = 1
 
         if cat is not None:
-            category_id = cat + (offset[use][0]
+            category_id = cat + offset[use][0]
             xbmc.executebuiltin('SetFocus({})'.format(category_id))
             if set is not None:
-                setting_id = set + (offset[use][1]
+                setting_id = set + offset[use][1]
                 xbmc.executebuiltin('SetFocus({})'.format(setting_id))
                 
                 if activate:
