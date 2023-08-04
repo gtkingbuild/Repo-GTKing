d42a70fad956509cc6c858b1888a1349f21684cf
gtkingbuild <gtkingbuild@gmail.com>
2023-08-04 10:30:25 +0200

    1

--- nexus/plugin.program.GTKing-Phoenix-Nexus/resources/libs/gui/build_menu.py
+++ nexus/plugin.program.GTKing-Phoenix-Nexus/resources/libs/gui/build_menu.py
@@ -192,7 +192,7 @@ class BuildMenu:
                                    icon=icon, themeit=CONFIG.THEME1)
                                    
             if themecheck:
-                directory.add_separator('[B]PARCHES GTKING PHOENIX TEAM[/B]', fanart=fanart, icon=icon, themeit=CONFIG.THEME2 )
+                directory.add_separator('[B]PARCHES GTKING PHOENIX[/B]', fanart=fanart, icon=icon, themeit=CONFIG.THEME2 )
 
                 response = tools.open_url(themefile)
                 theme = response.text
